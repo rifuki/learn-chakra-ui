0b470376d3b132d8b3585755419467fd5bceae04
rifuki <mahomarifuki@gmail.com>
2023-12-16 21:27:36 +0700

    course03: Flex Layouts

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,12 @@
-import { Heading, Text, Container, Box } from "@chakra-ui/react";
+
+import Navbar from "@/components/Navbar";
+import Dashboard from "@/components/Dashboard";
 
 export default function Home() {
-    const boxStyles = {
-        p: "10px",
-        bg: "purple.300",
-        color: "white",
-        m: "10px",
-        textAlign: "center",
-        filter:  'blur(2px)',
-        ':hover': {
-            color: 'black',
-            bg: 'blue.200',
-            cursor: 'pointer',
-            filter: 'none'
-        }
-    };
-
     return (
-        <Container maxWidth="2xl" py="20px" as="section"> {/* <- override div component as another component like section */}
-            <Heading my="30px" p="10px">Chakara UI Components</Heading>
-            <Text marginLeft="20px">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, laudantium?</Text>
-            <Text marginLeft="30px" color="blue.500" fontWeight="bold">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, laudantium?</Text>
-
-            <Box my="30px" p="50px" bg="orange">
-                <Text color="white">This is a box.</Text>
-            </Box>
-
-            <Box sx={boxStyles}>
-                Hello, ninjas!
-            </Box>
-        </Container>
+        <main>
+            <Navbar />
+            <Dashboard />
+        </main>
     );
 }
---
+++ components/Navbar.tsx
@@ -0,0 +1,25 @@
+// Course#03: Flex Layouts
+
+import { Flex, Heading, Box, Text, Button, Spacer, HStack } from '@chakra-ui/react';
+
+export default function Navbar() {
+    return(
+        <Flex as="nav" p="10px" alignItems="center">
+            <Heading as="h1">Chakra-Ui</Heading>
+            <Spacer />
+
+            <HStack spacing="20px">
+                <Box bg="gray.200" p="10px">R</Box>
+                <Text>Rifuki</Text>
+                <Button colorScheme='blue'>Logout</Button>
+            </HStack>
+        </Flex>
+
+        // <Flex bg="gray.200" justify="space-around" wrap="wrap-reverse" gap="3">
+        //     <Box width="150px" height="50px" bg="red">1</Box>
+        //     <Box width="150px" height="50px" flexGrow="2" bg="blue">2</Box>
+        //     <Box width="150px" height="50px" flexGrow="1" bg="green">3</Box>
+        //     <Box width="150px" height="50px" flexGrow="1" bg="yellow">4</Box>
+        // </Flex>
+    );
+}
