b82fb0a06b837102a84552c1c083711757c87ca3
rifuki <mahomarifuki@gmail.com>
2023-12-16 20:36:52 +0700

    course02: components & style props

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,34 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import { Heading, Text, Container, Box } from "@chakra-ui/react";
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    const boxStyles = {
+        p: "10px",
+        bg: "purple.300",
+        color: "white",
+        m: "10px",
+        textAlign: "center",
+        filter:  'blur(2px)',
+        ':hover': {
+            color: 'black',
+            bg: 'blue.200',
+            cursor: 'pointer',
+            filter: 'none'
+        }
+    };
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    return (
+        <Container maxWidth="2xl" py="20px" as="section"> {/* <- override div component as another component like section */}
+            <Heading my="30px" p="10px">Chakara UI Components</Heading>
+            <Text marginLeft="20px">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, laudantium?</Text>
+            <Text marginLeft="30px" color="blue.500" fontWeight="bold">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, laudantium?</Text>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+            <Box my="30px" p="50px" bg="orange">
+                <Text color="white">This is a box.</Text>
+            </Box>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+            <Box sx={boxStyles}>
+                Hello, ninjas!
+            </Box>
+        </Container>
+    );
 }
---
+++ components/providers/ChakraUiProvider.tsx
@@ -0,0 +1,8 @@
+import { ChakraProvider } from '@chakra-ui/react';
+import { ReactNode } from 'react';
+
+export default function ChakraUiProvider({children}:{children: ReactNode}) {
+    return (
+        <ChakraProvider>{children}</ChakraProvider>
+    )
+}
