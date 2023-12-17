1d98f8a7814e1af3f9be0e8314098d33477c68d8
rifuki <mahomarifuki@gmail.com>
2023-12-17 14:57:13 +0700

    course08: tabs component

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,3 +1,68 @@
+'use client'
+
+import { ChatIcon, CheckCircleIcon, EmailIcon, StarIcon, WarningIcon } from '@chakra-ui/icons';
+import { List, ListIcon, ListItem, Tab, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react';
+
 export default function Profile() {
-    return <div>Profile</div>;
+    return (
+        <Tabs
+            padding={{ base: '20px', md: '40px' }}
+            py={{ md: '20px' }}
+            colorScheme="purple"
+            variant="enclosed"
+        >
+            <TabList>
+                <Tab _selected={{color: 'white', bg: 'purple.400' }}>Account Info</Tab>
+                <Tab _selected={{color: 'white', bg: 'purple.400' }}>Post History</Tab>
+            </TabList>
+
+            <TabPanels>
+                <TabPanel>
+                    <List spacing={4}>
+                        <ListItem>
+                            <ListIcon as={EmailIcon} />
+                            Email: rifuki@nyan.moe
+                        </ListItem>
+                        <ListItem>
+                            <ListIcon as={ChatIcon}/>
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla labore molestias culpa porro totam incidunt fugiat.
+                        </ListItem>
+                        <ListItem>
+                            <ListIcon as={StarIcon} />
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi nihil, sequi perferendis hic corporis quae atque explicabo earum asperiores vel perspiciatis iure veniam nobis!
+                        </ListItem>
+                    </List>
+                </TabPanel>
+
+                <TabPanel>
+                    <List spacing={4}>
+                        <ListItem>
+                            <ListIcon as={CheckCircleIcon} color="teal.400" />
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                        </ListItem>
+                        <ListItem>
+                            <ListIcon as={CheckCircleIcon} color="teal.400" />
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                        </ListItem>
+                        <ListItem>
+                            <ListIcon as={WarningIcon} color="red.400" />
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                        </ListItem>
+                        <ListItem>
+                            <ListIcon as={CheckCircleIcon} color="teal.400" />
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                        </ListItem>
+                        <ListItem>
+                            <ListIcon as={WarningIcon} color="red.400" />
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                        </ListItem>
+                        <ListItem>
+                            <ListIcon as={CheckCircleIcon} color="teal.400" />
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                        </ListItem>
+                    </List>
+                </TabPanel>
+            </TabPanels>
+        </Tabs>
+    );
 }
